7c1e3a05facacff38818c1ddda4801b7e6cc9985
dmwix <87998561+dmwix@users.noreply.github.com>
2024-08-06 12:17:07 -0300

    animaciones

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ document.documentElement.style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
-  // let fullHeightMinusHeader = 100 * vh - headerHeight;
   eligen.style.minHeight = `${fullHeightMinusHeader}px`;
+  // let fullHeightMinusHeader = 100 * vh - headerHeight;
 });
 
 const body = document.querySelector("body");
@@ -25,29 +25,43 @@ document.documentElement.style.scrollPaddingTop = `${headerHeight}px`;
 // PARA MOBILE QUE SEA MÁS
 
 // FULLSCREEN MENU
-
-const fullscreenMenu = document.getElementById("fullscreen-menu");
-function openNav() {
-  fullscreenMenu.style.display = "flex";
-  body.style.overflowY = "hidden";
-  // HACER ESTO CON UNA CLASE EN CSS
-}
-
 const openMenu = document.getElementById("open-menu");
-openMenu.addEventListener("click", openNav);
+const closeMenu = document.getElementById("close-menu");
+const fullscreenMenu = document.getElementById("fullscreen-menu");
+
+function openNav() {
+  fullscreenMenu.classList.remove("animate__fadeOut");
+  fullscreenMenu.classList.add("animate__fadeIn");
+  body.classList.add("fullscreen-menu-opened");
+}
 
 function closeNav() {
-  document.getElementById("fullscreen-menu").style.display = "none";
-  body.style.overflowY = "visible";
+  fullscreenMenu.classList.add("animate__fadeOut");
+  fullscreenMenu.classList.remove("animate__fadeIn");
+  fullscreenMenu.addEventListener(
+    "animationend",
+    () => {
+      if (fullscreenMenu.classList.contains("animate__fadeOut")) {
+        body.classList.remove("fullscreen-menu-opened");
+      }
+    },
+    { once: true }
+  );
 }
 
-const closeMenu = document.getElementById("close-menu");
+openMenu.addEventListener("click", openNav);
 closeMenu.addEventListener("click", closeNav);
 
 const fullscreenMenuLinks = fullscreenMenu.querySelectorAll("a");
 fullscreenMenuLinks.forEach((a) => {
   a.addEventListener("click", closeNav);
 });
+
+const eligen = document.querySelector("#eligen");
+const izqSticky = document.querySelector("#izq-sticky");
+
+eligen.style.minHeight = `${fullHeightMinusHeader}px`;
+izqSticky.style.top = `${headerHeight + 80}px`;
 
 function addHeaderShadow() {
   if (window.scrollY >= 20) {
@@ -63,64 +77,14 @@ function addHeaderShadow() {
 
 window.addEventListener("scroll", addHeaderShadow);
 
-// window.addEventListener("hashchange", function () {
-//   window.scrollTo(window.scrollX, window.scrollY - headerHeight);
-// });
-
-// const counters = document.querySelectorAll(".value");
-// const speed = 250;
-
-// counters.forEach((counter) => {
-//   const animate = () => {
-//     const value = +counter.getAttribute("akhi");
-//     const data = +counter.innerText;
-
-//     const time = value / speed;
-//     if (data < value) {
-//       counter.innerText = Math.ceil(data + time);
-//       setTimeout(animate, 1);
-//     } else {
-//       counter.innerText = value;
-//     }
-//   };
-
-//   animate();
-// });
-
-// const counters = document.querySelectorAll(".value");
-// const duration = 900; // Total duration of the animation in milliseconds
-
-// counters.forEach((counter) => {
-//   const value = +counter.getAttribute("akhi");
-//   let startTime = null;
-
-//   const animate = (timestamp) => {
-//     if (!startTime) startTime = timestamp;
-//     const elapsed = timestamp - startTime;
-
-//     const progress = Math.min(elapsed / duration, 1);
-//     counter.innerText = Math.ceil(progress * value);
-
-//     if (progress < 1) {
-//       requestAnimationFrame(animate);
-//     } else {
-//       counter.innerText = value;
-//     }
-//   };
-
-//   requestAnimationFrame(animate);
-// });
-
 document.addEventListener("DOMContentLoaded", () => {
-  const counters = document.querySelectorAll(".counter-value");
-  const duration = 1000; // Total duration of the animation in milliseconds
-
   const formatNumber = (number) => {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   };
 
   const animateCounter = (counter) => {
     const value = +counter.getAttribute("akhi");
+    const duration = 1000;
     let startTime = null;
 
     const animate = (timestamp) => {
@@ -141,50 +105,48 @@ document.addEventListener("DOMContentLoaded", () => {
     requestAnimationFrame(animate);
   };
 
-  const observerOptions = {
+  // Setup Intersection Observer for counters
+  const counters = document.querySelectorAll(".counter-value");
+
+  const counterObserverOptions = {
     root: null, // Use the viewport as the root
     rootMargin: "0px",
-    threshold: 0.6, // Trigger when 10% of the element is visible
+    threshold: 0.6, // Trigger when 60% of the element is visible
   };
 
-  const observer = new IntersectionObserver((entries, observer) => {
+  const counterObserver = new IntersectionObserver((entries, observer) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         animateCounter(entry.target);
         observer.unobserve(entry.target); // Stop observing once the animation has started
       }
     });
-  }, observerOptions);
+  }, counterObserverOptions);
 
   counters.forEach((counter) => {
-    observer.observe(counter);
+    counterObserver.observe(counter);
   });
+
+  const beneficiosLista = document.querySelector("#beneficios-lista");
+  const listItems = beneficiosLista.querySelectorAll("li");
+
+  const listObserverOptions = {
+    root: null, // Use the viewport as the root
+    rootMargin: "0px",
+    threshold: 0.3, // Trigger when 100% of the element is visible
+  };
+
+  const listObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        listItems.forEach((item, index) => {
+          item.classList.add("show", "animate__animated", "animate__fadeIn");
+          item.style.animationDelay = `${index * 200}ms`;
+        });
+        listObserver.unobserve(beneficiosLista);
+      }
+    });
+  }, listObserverOptions);
+
+  listObserver.observe(beneficiosLista);
 });
-
-const eligen = document.querySelector("#eligen");
-const izqFijo = document.querySelector("#izq-fijo");
-
-eligen.style.minHeight = `${fullHeightMinusHeader}px`;
-izqFijo.style.top = `${headerHeight + 80}px`;
-
-// function halfSticky() {
-//   if (window.scrollY >= diferentesPosition) {
-//     izqFijo.style.position = "sticky";
-//     izqFijo.style.top = `${diferentesPosition + headerHeight}px`;
-//   }
-// }
-
-// window.addEventListener("scroll", halfSticky);
-
-// window.addEventListener("scroll", function () {
-//   let izqFijo = document.querySelector("#izq-fijo");
-//   let rightColumn = document.querySelector("#derecha-scroll");
-//   let rightColumnBottom = rightColumn.getBoundingClientRect().bottom;
-
-//   if (rightColumnBottom <= headerHeight) {
-//     izqFijo.style.position = "relative";
-//   } else {
-//     izqFijo.style.position = "sticky";
-//     izqFijo.style.top = `${headerHeight + 20}px`;
-//   }
-// });
